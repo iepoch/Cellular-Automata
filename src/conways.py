10fd9a7a7b7993f692109937280a38d39409b54a
Stefan <steaclem@gmail.com>
2019-08-13 20:32:33 -0700

    Refactor and changed a few things. Trying to radomize it as a grid.

--- src/conways.py
+++ src/conways.py
@@ -5,99 +5,102 @@ import random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (25, 25, 25)
-WIN_SIZE = WIDTH, HEIGHT = 500, 500
+WIN_SIZE = 500
 
+CELL_SIZE = 20
+# WIN_SIZE = WIDTH, HEIGHT = 500, 500
 
-class LifeGame:
+# Create an initial states.
+cur_states = [0] * 400
+cur_states[10] = 1
+cur_states[30] = 1
+cur_states[50] = 1
 
-    def __init__(self):
-        pygame.init()
+next_states = []
 
-        self.screen = pygame.display.set_mode(WIN_SIZE)
+num_cols = int(WIN_SIZE / CELL_SIZE)
+num_rows = int(WIN_SIZE / CELL_SIZE)
+print(f'Columns: {num_cols} \nRows: {num_rows}')
+grids = [
+    [[0] * num_rows] * num_cols,
+    [[0] * num_rows] * num_cols
+]
+active_grid = 0
 
-        # Add a title
-        pygame.display.set_caption("Conway's Game of Life")
+# Set the width and height of the screen [width, height]
+size = (WIN_SIZE, WIN_SIZE)
+screen = pygame.display.set_mode(size)
 
-    def run(self):
+# Add a title
+pygame.display.set_caption("Conway's Game of Life")
 
-        # Create an initial states.
-        cur_states = [0] * 400
-        cur_states[10] = 1
-        cur_states[30] = 1
-        cur_states[50] = 1
+# Loop until the user clicks the close button.
+done = False
 
-        next_states = []
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
 
-    # previous_state=[]
-        # Loop until the user clicks the close button.
-        done = False
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
-        # Used to manage how fast the screen updates
-        clock = pygame.time.Clock()
+   # --- Game logic should go here
 
-        # -------- Main Program Loop -----------
-        while not done:
-            # --- Main event loop
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    done = True
-            self.screen.fill(BLACK)
+        # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        # Any live cell with two or three live neighbours lives on to the next generation.
+        # Any live cell with more than three live neighbours dies, as if by overpopulation.
+        # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-    # --- Game logic should go here
+        # 3. Work on rules that  i) look at all neighnors, save the new_states
+        # if cell == 1:
+        #     if sum_of_neighbors < 2:
+        #         return 0
+        #     elif sum_of_neighbors < 4:
+        #         return 1
+        #     else:
+        #         return 0
+        # else:
+        #     if sum_of_neighbors == 3:
+        #         return 1
+        #     else:
+        #         return 0
 
-# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by overpopulation.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        # --- Screen-clearing code goes here
 
-# 3. Work on rules that  i) look at all neighnors, save the new_states
-    # if cell == 1:
-    #     if sum_of_neighbors < 2:
-    #         return 0
-    #     elif sum_of_neighbors < 4:
-    #         return 1
-    #     else:
-    #         return 0
-    # else:
-    #     if sum_of_neighbors == 3:
-    #         return 1
-    #     else:
-    #         return 0
+        # Here, we clear the screen to gray. Don't put other drawing commands
+        # above this, or they will be erased with this command.
+    screen.fill(GRAY)
 
-    # --- Screen-clearing code goes here
+    # --- Drawing code should go here
+    # Randomize the Grid
+    print(grids[0])
+    for c in range(num_cols):
+        for r in range(num_rows):
+            grids[active_grid][c][r] = random.choice([0, 1])
+    cur_index = 0
+    x = 3.5
+    while x < WIN_SIZE:
+        y = 3.2
+        while y < WIN_SIZE:
+            state = cur_states[cur_index]
+            # 2. Draw  based on vlaues of current_state
+            if state == 0:
+                pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, 20, 20))
+            else:
+                pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, 20, 20))
 
-    # Here, we clear the screen to gray. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-        self.screen.fill(GRAY)
+                cur_index + 1
+            y += 25
+        x += 25
 
-        # --- Drawing code should go here
-        cur_index = 0
-        x = 5
-        while x < WIN_SIZE:
-            y = 5
-            while y < WIN_SIZE:
-                state = cur_states[cur_index]
-                # 2. Draw  based on vlaues of current_state
-                if state == 0:
-                    pygame.draw.rect(self.screen, WHITE,
-                                     pygame.Rect(x, y, 20, 20))
-                else:
-                    pygame.draw.rect(self.screen, BLACK,
-                                     pygame.Rect(x, y, 20, 20))
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
 
-                    cur_index + 1
-                y += 25
-            x += 25
+    # --- Limit to 5 frames per second
+    clock.tick(5)
 
-        # --- Go ahead and update the screen with what we've drawn.
-        pygame.display.flip()
-
-        # --- Limit to 5 frames per second
-        clock.tick(5)
-
-
-if __name__ == '__main__':
-    game = LifeGame()
-    game.run()
 # Close the window and quit.
 pygame.quit()
